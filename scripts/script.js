9f747d58ec36caefa1a8e21207fe02c1b7a3772f
grant2695 <92463829+grant2695@users.noreply.github.com>
2022-02-01 18:55:14 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,14 @@
 window.addEventListener ('load',(event)=>{
+  header ("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header ("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header ("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$ method = $ _SERVER ['REQUEST_METHOD'];
+if ($ method == "OPTIONS") {
+     die ();
+}
+  
+  
+  
   console.log('page has loaded')
   const api_key = "cEpjCVyhxbYkyc40rcSJkfWfbjrU3moX"
 
